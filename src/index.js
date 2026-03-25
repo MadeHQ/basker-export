182b11a6f4bf0ad23633aeb27c12bc577e41b391
Hiren Patel <hiren.patel@made.media>
2026-03-25 12:17:26 +0000

    Use `patternlabl-build-end` event to generate zip/link instead of write end which writes constantly

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const { execSync } = require('child_process');
 const archiver = require('archiver');
 
 const pluginName = '@madehq/pl-basker-export';
-const safePluginName = '@madehq-pl-basker-export';
 
 function debounce(func, timeout = 300) {
   let timer;
@@ -15,46 +14,7 @@ function debounce(func, timeout = 300) {
   };
 }
 
-module.exports = function (patternlab) {
-  if (!patternlab) {
-    process.exit(1);
-  }
-
-  if (pluginName in patternlab.config.plugins === false) {
-    return;
-  }
-
-  if (patternlab.config.plugins[pluginName].enabled !== true) {
-    return;
-  }
-
-  if (patternlab.config.plugins[pluginName].initialized === true) {
-    return;
-  }
-
-  const delay = parseInt(patternlab.config.plugins[pluginName].debounce, 10) || 2500;
-
-  patternlab.events.on('patternlab-pattern-write-end', debounce(() => {
-    addDownloadLink();
-    generateExportZip();
-  }, delay));
-
-  patternlab.config.plugins[pluginName].initialized = true;
-}
-
-async function addDownloadLink() {
-  const plViewerFile = 'public/styleguide/js/patternlab-viewer.modern.js';
-
-  let plViewerContent = readFileSync(plViewerFile, 'utf8');
-
-  if (plViewerContent.indexOf('Basker Export') === -1) {
-    plViewerContent = plViewerContent.replace(/<li class="pl-c-tools__item">/, '<li class="pl-c-tools__item"><a class="pl-c-button pl-c-button--medium" href="/basker-export.zip"><span class="pl-c-button__text">Basker Export</span><span class="pl-c-button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" style="width: 1.2em; height: 1.2em"><path fill="none" d="M0 0h48v48H0z"/><path d="M22 4v16h-8l10 10 10-10h-8V4zM8 44h32c2.206 0 4-1.794 4-4V30h-4v10H8V30H4v10c0 2.206 1.794 4 4 4z"/></svg></span></a></li><li class="pl-c-tools__item">');
-
-    writeFileSync(plViewerFile, plViewerContent, 'utf8');
-  }
-}
-
-const generateExportZip = debounce(function () {
+const generateExportZip = function () {
   /**
    * Create `export` directory with following structure
    * (see https://basker.dev/themes/architecture/overview#directory-structure-and-component-types)
@@ -91,8 +51,10 @@ const generateExportZip = debounce(function () {
     console.warn('Could not determine theme name from "settings_schema.json" - have you defined it? Using "basker-export.zip" instead');
   }
 
+  const filename = `${name}_${getBranchName()}.zip`;
+
   // Generate the ZIP Export from the `export` directory
-  const output = createWriteStream(`public/${name}_${getBranchName()}.zip`);
+  const output = createWriteStream(`public/${filename}`);
 
   const archive = archiver('zip', {
       zlib: 9,
@@ -103,7 +65,6 @@ const generateExportZip = debounce(function () {
   output.on('close', function() {
       console.log(archive.pointer() + ' total bytes for export');
   });
-
 
   // good practice to catch warnings (ie stat failures and other non-blocking errors)
   archive.on('warning', function(err) {
@@ -129,12 +90,72 @@ const generateExportZip = debounce(function () {
   // finalize the archive (ie we are done appending files but streams have to finish yet)
   // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
   archive.finalize();
-}, 2500);
 
-function getBranchName() {
+  return filename;
+}
+
+const getBranchName = () => {
   try {
     return execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf8' }).trim().replace(/^.*\//, '').toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '');
   } catch (e) {
     return 'unknown';
   }
+}
+
+async function addDownloadLink(filename) {
+  const plViewerFile = 'public/styleguide/js/patternlab-viewer.modern.js';
+
+  let plViewerContent = readFileSync(plViewerFile, 'utf8');
+
+  if (plViewerContent.indexOf('Basker Export') === -1) {
+    const linkMarkup = `
+      <li class="pl-c-tools__item">
+          <a class="pl-c-button pl-c-button--medium" href="/${filename}">
+              <span class="pl-c-button__text">Basker Export</span>
+              <span class="pl-c-button__icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" style="width: 1.2em; height: 1.2em">
+                      <path fill="none" d="M0 0h48v48H0z" />
+                      <path d="M22 4v16h-8l10 10 10-10h-8V4zM8 44h32c2.206 0 4-1.794 4-4V30h-4v10H8V30H4v10c0 2.206 1.794 4 4 4z" />
+                  </svg>
+              </span>
+          </a>
+      </li>
+      <li class="pl-c-tools__item">
+    `;
+
+    plViewerContent = plViewerContent.replace('<li class="pl-c-tools__item">', linkMarkup);
+
+    writeFileSync(plViewerFile, plViewerContent, 'utf8');
+  }
+}
+
+module.exports = function (patternlab) {
+  if (!patternlab) {
+    process.exit(1);
+  }
+
+  if (pluginName in patternlab.config.plugins === false) {
+    return;
+  }
+
+  if (patternlab.config.plugins[pluginName].enabled !== true) {
+    return;
+  }
+
+  if (patternlab.config.plugins[pluginName].initialized === true) {
+    return;
+  }
+
+  const delay = parseInt(patternlab.config.plugins[pluginName].debounce, 10) || 2500;
+
+  patternlab.events.on('patternlab-build-end', debounce(() => {
+    try {
+      const filename = generateExportZip();
+      addDownloadLink(filename);
+    } catch (e) {
+      throw e;
+    }
+  }, delay));
+
+  patternlab.config.plugins[pluginName].initialized = true;
 }
